7c0b2660c4a4678f9426ba0b6c065f1e8f7b7948
Zayed <zayed.cs3@gmail.com>
2021-01-11 11:01:29 +0600

    Hospital Filter by Location Added.

--- services/views/hospital_view.py
+++ services/views/hospital_view.py
@@ -3,12 +3,28 @@ from django.views import View
 
 from address.models import District
 from services.models.hospital_models import Hospital
+from utils.filter import filter_by_address
 
 
 class HospitalList(View):
     def get(self, request):
         districts = District.objects.all()
         hospitals = Hospital.objects.all()
+        context = {
+            'districts': districts,
+            'hospitals': hospitals,
+        }
+        return render(request, 'services/hospital/list.html', context)
+
+    def post(self, request):
+        data = request.POST
+        location = data.get('location')
+        district = data.get('district')
+        zip_code = data.get('zip_code')
+
+        districts = District.objects.all()
+        hospitals = filter_by_address(Hospital, location, district, zip_code)
+
         context = {
             'districts': districts,
             'hospitals': hospitals,
