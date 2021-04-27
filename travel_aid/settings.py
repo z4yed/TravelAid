7db68b2600cc33d4a1b67e2bb3db4a8d8a140568
Zayed <zayed.cs3@gmail.com>
2021-04-27 10:02:16 +0600

    Postgres Hobby Dev Connection added.

--- travel_aid/settings.py
+++ travel_aid/settings.py
@@ -100,6 +100,10 @@ DATABASES = {
     }
 }
 
+# postgres conf of Hobby Dev
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=600)
+DATABASES['default'].update(db_from_env)
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
